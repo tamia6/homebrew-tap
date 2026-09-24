691176a73873248335d98bd4d712343716c3ad34
tamia <tamia.here@gmail.com>
2026-09-24 16:06:57 +0800

    Update rfig to v0.1.2

--- Formula/rfig.rb
+++ Formula/rfig.rb
@@ -1,8 +1,8 @@
 class Rfig < Formula
   desc "Inline terminal command completion for zsh"
   homepage "https://github.com/tamia6/rfig"
-  url "https://github.com/tamia6/rfig/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "e99d1020e1004f39f72a734172786a67b8ca9f2dc75372a6cccded3ac98a3902"
+  url "https://github.com/tamia6/rfig/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "ff96b5d80f65ca6db5b0f39482bc0dc8253c4cd2c3ea2344bc47c626f0eac78a"
   license "MIT"
 
   depends_on "rust" => :build
